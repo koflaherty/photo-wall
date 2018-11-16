1771323d4cfdff7bb279d7716688018588acf20f
Kevin O'Flaherty <koflaherty@ymail.com>
2018-11-15 19:48:28 -0500

    Set up a basic request to giphy

--- src/components/photo-wall/photo-wall.jsx
+++ src/components/photo-wall/photo-wall.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import giphy from "../../data/giphy/giphy";
+import { PHOTO_KEYWORDS } from "../../constants/constants";
 
 class PhotoWall extends Component {
     constructor() {
@@ -7,15 +8,16 @@ class PhotoWall extends Component {
     }
 
     componentDidMount() {
-        giphy().search('gifs', {"q": "cats"})
-            .then((response) => {
-                response.data.forEach((gifObject) => {
-                    console.log(gifObject)
-                })
-            })
-            .catch((err) => {
-                console.log("Need to handle error");
-            })
+        giphy().search('gifs', {
+            "q": Math.floor( Math.random() * PHOTO_KEYWORDS.length )
+        }).then((response) => {
+            response.data.forEach((gifObject) => {
+                console.log(gifObject)
+            });
+        })
+        .catch((err) => {
+            console.log("Need to handle error");
+        })
     }
 
     render() {
---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+export const PHOTO_KEYWORDS  = [
+    'too much coffee',
+    'super star',
+    'puppies',
+    'sad sea turtles',
+    'chocolate'
+];
