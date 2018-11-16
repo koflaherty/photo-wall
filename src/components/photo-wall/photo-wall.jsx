566333e332acc5d649b7a8bea57e72cbb89820e2
Kevin O'Flaherty <koflaherty@ymail.com>
2018-11-16 12:05:58 -0500

    Expiremented with handeling loading

--- src/components/photo-wall/photo-wall.jsx
+++ src/components/photo-wall/photo-wall.jsx
@@ -14,15 +14,27 @@ class PhotoWall extends Component {
             searchWord: PHOTO_KEYWORDS[ Math.floor( Math.random() * PHOTO_KEYWORDS.length )],
         };
 
+        this.page = 0;
+        this.currentlyLoading = false;
+        this.wantsToLoadMore = false;
+
         this.loadMoreGiphys = this.loadMoreGiphys.bind(this);
     }
 
-    loadMoreGiphys(page) {
-        console.log("Loading " + page * 100);
+    loadMoreGiphys() {
+        // Prevent Loading Multiple Requests
+        if (this.currentlyLoading) {
+            this.wantsToLoadMore = true;
+            return;
+        }
+
+        console.log("Attempt: " + this.page * 100);
+
+        this.currentlyLoading = true;
         giphy().search('gifs', {
             "q": this.state.searchWord,
             limit: 100,
-            offset: page * 100,
+            offset: this.page * 100,
         }).then((response) => {
             const photos = response.data.map((photo) => {
                 const giphyImageData = photo.images['fixed_height_small'];
@@ -34,16 +46,25 @@ class PhotoWall extends Component {
                 };
             });
 
-            console.log("LOADED " + page * 100);
+            console.log("LOADED " + this.page * 100);
+            this.page += 1;
 
             this.setState({ photos: this.state.photos.concat(photos) });
         })
         .catch((err) => {
-            console.log("Error " + page * 100);
+            this.currentlyLoading = false;
+
+            this.setState({ photos: this.state.photos.concat([]) }); // hack to update infinite loader
+            console.log("Error " + this.page * 100);
             console.log(err);
-            this.setState({
-                photos: this.state.photos.concat([])
-            })
+        }).finally(() => {
+            console.log("FINALLY");
+            this.currentlyLoading = false;
+
+            if (this.wantsToLoadMore) {
+                this.wantsToLoadMore = false;
+                this.loadMoreGiphys();
+            }
         });
     }
 
@@ -58,18 +79,18 @@ class PhotoWall extends Component {
         });
 
         return (
-            <InfiniteScroll
-                hasMore={true}
-                loadMore={this.loadMoreGiphys}
-                loader={<div className="loader">Loading ...</div>}
-                threshold={100}
-                style={ { minHeight: '100vh'} }
-            >
+            <div>
                 <h1>{ this.state.searchWord }</h1>
-                <div>
+                <InfiniteScroll
+                    hasMore={true}
+                    loadMore={this.loadMoreGiphys}
+                    loader={<div className="loader">Loading ...</div>}
+                    threshold={100}
+                    style={ { minHeight: '100vh'} }
+                >
                     { photos }
-                </div>
-            </InfiniteScroll>
+                </InfiniteScroll>
+            </div>
         )
     }
 }
